f65a967f0fb88c05a737fa06ecbe43d811d03e98
petch24 <57621866+petch24@users.noreply.github.com>
2020-03-29 18:37:13 +0700

    Create component

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -13,7 +13,7 @@ class Usercontroller extends Controller
      */
     public function index()
     {
-        //
+        return view('users');
     }
 
     /**
---
+++ resources/js/components/User.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>User Component</h1>
+      <h1>Laravel Vue.js</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
